170f9bae9ac095918da9a468541ce66b1c1c9f0e
SushantSeven <sushant.das166@gmai.com>
2024-03-10 21:50:20 +0530

    [Sushant] Add: Python Script to check if it is a valid email

--- emp.py
+++ emp.py
@@ -1,30 +1,32 @@
+import re
 # method to check if the first name is valid
 def is_valid_first_name(first_name):
 
-    return 0 if first_name[0].isupper() and len(first_name)>=3 else 1 # return 0 if first name is valid else 1
+    return 0 if first_name[0].isupper() and len(first_name)>=3 else 1
 
 # method to check if the last name is valid
 def is_valid_last_name(first_name, last_name):
-#    method to check first name is called
+#    method to check first name is called 
     return (is_valid_first_name(first_name), 0) if last_name[0].isupper() and len(last_name) >= 3 else (is_valid_first_name(first_name), 1)
+
+# method to check if the email is valid or not
+def is_valid_email(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+\.+[a-zA-Z]+@[bi]+\.co(?:\.[a-zA-Z]{2})?$' # creating pattern to check the email
+    if re.match(pattern, email): # using regex.match 
+        return True
+    else:
+        return False
 
 # main function
 def main():
-    first_name = input("Enter your first name: ") # variable to store the first name
-    last_name = input("Enter your last name: ") # variable to store the last name
-    result = is_valid_last_name(first_name, last_name) # variable to hosd the value returned by the is_valid_firstname function
-    if result == (0,0):
-        print("valid first name and last name")
-    elif result == (0,1):
-        print("valid first name and invalid last name")
-    elif result == (1,0):
-        print("Invalid firstname and valid last name")
-    elif result == (1,1):
-        print("Invalid firstname and last name")
+    while True: # loop until you enter a valid email
+        email = input("Enter your email address: ")
+        if is_valid_email(email):
+            print("Valid email address entered:", email)
+            break
+        else:
+            print("Invalid email address. Please try again.")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-    
-    
-    
