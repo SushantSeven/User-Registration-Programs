a47ebfa9b5816153de924673ee4590ea525ab5a6
SushantSeven <sushant.das166@gmai.com>
2024-03-11 11:50:32 +0530

    [Sushant] Refactor: used regex for validating first name

--- emp.py
+++ emp.py
@@ -1,11 +1,13 @@
+import re
 # method to check if the first name is valid
 def is_valid_First_name(first_name):
-    return 0 if first_name[0].isupper() and len(first_name)>=3 else 1 # returns 0 if first name is valid else 1
+    first_name_patttern = r'^[A-Z]'
+    return bool(re.match(first_name_patttern,first_name)) # returns true if the first name is valid else false
 
 # main function
 def main():
     result = is_valid_First_name(input("Enter your first name : ")) # variable to hold the value returned by the is_valid_firstname function
-    print("It is a valid first name") if result == 0 else print("It is not a valid first name")
+    print("It is a valid first name") if result is True else print("It is not a valid first name")
 
 
 if __name__=="__main__":
