4c9ae2a510a898a9513ab81f2c0fa2f9263f7207
SushantSeven <sushant.das166@gmai.com>
2024-03-11 12:35:58 +0530

    [Suhant] Refactor: Modified the regex pattern to validate the last name of the user

--- emp.py
+++ emp.py
@@ -1,27 +1,20 @@
+import re
 # method to check if the first name is valid
 def is_valid_first_name(first_name):
+        first_name_patttern = r'^[A-Z]{1}+[a-z]{2,}'
+        return bool(re.match(first_name_patttern,first_name)) # returns true if the first name is valid else false
 
-    return 0 if first_name[0].isupper() and len(first_name)>=3 else 1 # return 0 if first name is valid else 1
 
 # method to check if the last name is valid
-def is_valid_last_name(first_name, last_name):
-#    method to check first name is called
-    return (is_valid_first_name(first_name), 0) if last_name[0].isupper() and len(last_name) >= 3 else (is_valid_first_name(first_name), 1)
+def is_valid_last_name(last_name):
+     last_name_patttern = r'^[A-Z]{1}+[a-z]{2,}'
+     return bool(re.match(last_name_patttern,last_name)) # returns true if the first name is valid else false
 
 # main function
 def main():
-    first_name = input("Enter your first name: ") # variable to store the first name
     last_name = input("Enter your last name: ") # variable to store the last name
-    result = is_valid_last_name(first_name, last_name) # variable to hosd the value returned by the is_valid_firstname function
-    if result == (0,0):
-        print("valid first name and last name")
-    elif result == (0,1):
-        print("valid first name and invalid last name")
-    elif result == (1,0):
-        print("Invalid firstname and valid last name")
-    elif result == (1,1):
-        print("Invalid firstname and last name")
-
+    print("It is a valid last name! ")if is_valid_last_name(last_name) is True else print("It is not a valid last name!")
+    
 
 if __name__=="__main__":
     main()
