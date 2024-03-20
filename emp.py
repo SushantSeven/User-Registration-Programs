bbc8e8a2bdb3c8190c01f502294b376f26b14b6d
SushantSeven <sushant.das166@gmai.com>
2024-03-20 09:13:29 +0530

    [Sushant] Add: Refactored the code to pass all test cases

--- emp.py
+++ emp.py
@@ -1,25 +1,26 @@
 import re
 # method to check if the first name is valid
 def is_valid_first_name(first_name):
-     first_name_patttern = r'^[A-Z]{1}+[a-z]{2,}'
+     first_name_patttern = r'^[A-Z][a-zA-Z]{2,}$'
      return bool(re.match(first_name_patttern,first_name)) # returns true if the first name is valid else false
 
 # method to check if the last name is valid
 def is_valid_last_name(last_name):
-      last_name_patttern = r'^[A-Z]{1}+[a-z]{2,}'
+      last_name_patttern = r'^[A-Z][a-zA-Z]{2,}$'
       return bool(re.match(last_name_patttern,last_name)) # returns true if the first name is valid else false
 
 # method to check if the email is valid or not
 def is_valid_email(email):
-    pattern = r'^[\w.-]+@([\w-]+\.[\w.-]+)$' # creating pattern to check the email
-    if re.match(pattern, email): # using regex.match 
+    pattern = r'^[\w+.-]+@([\w-]+\.[\w-]{2,}+)$' # creating pattern to check the email
+    double_dot = r'\.\.'
+    if re.match(pattern, email) and len(re.findall(double_dot, email))==0: # using regex.match 
         return True
     else:
         return False
 
 # method to validate phone number
 def is_valid_phone_no(phone_number):
-    phone_pattern = r'^91\s{1}[0-9]{10}$' # creat a pattern to chcke a valid phone number
+    phone_pattern = r'^91\s{1}[7,6,8,9]{1}[0-9]{9}$' # creat a pattern to chcke a valid phone number
     return True if re.match(phone_pattern, phone_number) else False # return true if phone number is valid else False
 
 # method to validate password
